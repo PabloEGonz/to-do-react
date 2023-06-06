58d239336e993f0a1953eb3a09edbc240fcc8fe0
Pablo <ezekielgonzalezr@gmail.com>
2023-06-05 22:06:03 -0600

    Use icons

--- src/modules/ToDoItem.js
+++ src/modules/ToDoItem.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import { FaTrash } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
+
+
 
 export default function ToDoItem({ tasks, setTasks, deleteTask, setUpdate }) {
     const complete = (id) => {
@@ -34,8 +38,8 @@ export default function ToDoItem({ tasks, setTasks, deleteTask, setUpdate }) {
                 <input type='checkbox' checked={tasks.completed} onChange={() => complete(tasks.id)} />
                 <p className={crossed}>{tasks.title}</p>
                 <div className='flex buttons'>
-                    <button onClick={editing}>Edit</button>
-                    <button onClick={() => deleteTask(tasks.id)}>Delete</button>
+                    <button onClick={editing}><AiFillEdit className='icon' /></button>
+                    <button onClick={() => deleteTask(tasks.id)}><FaTrash className='icon' /></button>
                 </div>
             </div>
             <input className={editmode} type='text' value={tasks.title} onChange={(e) => setUpdate(e.target.value, tasks.id)} onKeyDown={save} />
