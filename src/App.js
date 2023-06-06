82824f86f7e7a31386c1b495bfcefb4fba92409b
Pablo <ezekielgonzalezr@gmail.com>
2023-06-05 21:33:39 -0600

    Add sections to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-
+import Header from './modules/header';
+import TodoLogic from './modules/TodoLogic';
 function App() {
   return (
     <div className="App">
+      <Header />
+      <TodoLogic />
     </div>
   );
 }
